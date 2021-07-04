fbd92641d53229cbb9309e60e9aabb0cce460f7c
impel-star <panbing1101@163.com>
2021-07-04 20:41:59 +0800

    https://www.cnblogs.com/qdhxhz/

---
+++ src/main/java/com/service/RocketMqService.java
@@ -0,0 +1,12 @@
+package com.service;
+
+/**
+ * url:https://www.cnblogs.com/qdhxhz/
+ * @Author Administrator
+ * @Date 2021/7/4 0004 20:31
+ * @Description:
+ * @Version 1.0
+ */
+public class RocketMqService {
+    
+}
